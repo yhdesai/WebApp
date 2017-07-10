bf3a0f469e1771431b083269b778d0c3e37dbad2
Yhdesai <yhdesai99@ymail.com>
2017-07-10 11:56:28 +0530

    Initial commit

---
+++ app/src/main/java/io/github/yhdesai/webapp/MyAppWebViewClient.java
@@ -0,0 +1,23 @@
+package io.github.yhdesai.webapp;
+
+/**
+ * Created by Yash on 7/10/2017.
+ */
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyAppWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (Uri.parse(url).getHost().endsWith("example.com")) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
