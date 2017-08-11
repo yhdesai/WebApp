e1110946b69268dd10246d4c8f4538f57a7c5558
yash <yhdesai99@ymail.com>
2017-08-11 14:07:33 +0530

    added internet perms

--- app/src/main/java/io/github/yhdesai/webapp/MainActivity.java
+++ app/src/main/java/io/github/yhdesai/webapp/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends Activity {
 
 
 
-       // getMenuInflater().inflate(R.menu.menu_main, menu);
+       //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
